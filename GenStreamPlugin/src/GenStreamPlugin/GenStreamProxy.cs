b5cc75b5576a5abb79a4d47e12891028e654a3a9
Andrei Laperie <andrei.laperie@loupedeck.com>
2022-10-26 19:07:16 +0300

    Added support for Scene Collections and added #regions

--- GenStreamPlugin/src/GenStreamPlugin/GenStreamProxy.cs
+++ GenStreamPlugin/src/GenStreamPlugin/GenStreamProxy.cs
@@ -1,6 +1,7 @@
 ﻿namespace Loupedeck.GenStreamPlugin
 {
     using System;
+    using System.Collections.Generic;
     using OBSWebsocketDotNet;
 
 
@@ -35,7 +36,7 @@
         //FIXME: Provide customized images for starting/started... -- For that, create special event handler on Action side. 
         private void OnObsRecordingStateChange(OBSWebsocket sender, OBSWebsocketDotNet.Types.OutputState newState)
         {
-            if( (newState == OBSWebsocketDotNet.Types.OutputState.Started )  || (newState == OBSWebsocketDotNet.Types.OutputState.Starting) )
+            if ((newState == OBSWebsocketDotNet.Types.OutputState.Started) || (newState == OBSWebsocketDotNet.Types.OutputState.Starting))
             {
                 this.AppEvtRecordingOn?.Invoke(this, new EventArgs());
             }
@@ -52,10 +53,7 @@
             }
         }
 
-
-        //
-        // STREAMING
-        //
+        #region STREAMING TOGGLE
         public event EventHandler<EventArgs> AppEvtStreamingOn;
         public event EventHandler<EventArgs> AppEvtStreamingOff;
 
@@ -77,10 +75,9 @@
                 Helpers.TryExecuteSafe(() => this.ToggleStreaming());
             }
         }
+        #endregion
 
-        //
-        // VIRTUAL CAM
-        //
+        #region VIRTUAL CAM TOGGLE
         public event EventHandler<EventArgs> AppEvtVirtualCamOn;
         public event EventHandler<EventArgs> AppEvtVirtualCamOff;
 
@@ -94,10 +91,9 @@
                 Helpers.TryExecuteSafe(() => this.ToggleVirtualCam());
             }
         }
+        #endregion
 
-        //
-        // STUDIO MODE
-        //
+        #region STUDIO MODE TOGGLE
         public event EventHandler<EventArgs> AppEvtStudioModeOn;
         public event EventHandler<EventArgs> AppEvtStudioModeOff;
 
@@ -119,10 +115,9 @@
                 Helpers.TryExecuteSafe(() => this.ToggleStudioMode());
             }
         }
+        #endregion
 
-        //
-        // REPLAY BUFFER
-        //
+        #region REPLAY BUFFER TOGGLE
         public event EventHandler<EventArgs> AppEvtReplayBufferOn;
         public event EventHandler<EventArgs> AppEvtReplayBufferOff;
         private void OnObsReplayBufferStateChange(OBSWebsocket sender, OBSWebsocketDotNet.Types.OutputState newState)
@@ -144,10 +139,58 @@
                 Helpers.TryExecuteSafe(() => this.ToggleReplayBuffer());
             }
         }
+        #endregion
+
+        #region SCENE COLLECTIONS
+
+        public event EventHandler<EventArgs> AppEvtSceneCollectionsChanged;
+        public event EventHandler<EventArgs> AppEvtCurrentSceneCollectionChanged;
+
+
+        public List<String> SceneCollections { get; private set; }
+
+        public String CurrentSceneCollection {  get; private set; }
+
+        void OnObsSceneCollectionListChanged(Object sender, EventArgs e)
+        {
+            if( Helpers.TryExecuteSafe(() => { this.SceneCollections = this.ListSceneCollections(); }) )
+            { 
+                this.AppEvtSceneCollectionsChanged?.Invoke(sender, e);
+            }
+        }
+
+        void OnObsSceneCollectionChanged(Object sender, EventArgs e)
+        {
+            if( Helpers.TryExecuteSafe(() => { this.CurrentSceneCollection = this.GetCurrentSceneCollection(); }) )
+            {
+                this.AppEvtCurrentSceneCollectionChanged?.Invoke(sender, e);
+            }
+        }
+
+        public void CycleSceneCollections()
+        {
+            if(this.IsAppConnected && this.SceneCollections != null && this.CurrentSceneCollection!="")
+            {
+                var index = this.SceneCollections.IndexOf(this.CurrentSceneCollection);
+                index = index != this.SceneCollections.Count - 1 ? index + 1 : 0;
+                this.AppSwitchToScenCollection(this.SceneCollections[index]);
+            }
+        }
+
+        public void AppSwitchToScenCollection(String newCollection)
+        {
+            if (this.IsAppConnected && this.SceneCollections.Contains(newCollection) && this.CurrentSceneCollection != newCollection)
+            {
+                Helpers.TryExecuteSafe(() => this.SetCurrentSceneCollection(newCollection));
+            }
+
+        }
+        #endregion
         // ----------------------------------
         private void OnAppConnected(Object sender, EventArgs e)
         {
 
+            
             // Subscribing to App events
             // Notifying all subscribers on App Connected
             // Fetching initial states for controls
@@ -157,6 +200,10 @@
             this.VirtualCameraStopped  += this.OnObsVirtualCameraStopped;
             this.StudioModeSwitched += this.OnObsStudioModeStateChange;
             this.ReplayBufferStateChanged += this.OnObsReplayBufferStateChange;
+
+            this.SceneCollectionListChanged += this.OnObsSceneCollectionListChanged;
+            this.SceneCollectionChanged += this.OnObsSceneCollectionChanged;
+
 
             this.EvtAppConnected?.Invoke(sender, e);
 
@@ -172,7 +219,6 @@
                     this.OnObsRecordingStateChange(this, streamingStatus.IsRecording ? OBSWebsocketDotNet.Types.OutputState.Started : OBSWebsocketDotNet.Types.OutputState.Stopped);
                     this.OnObsStreamingStateChange(this, streamingStatus.IsStreaming ? OBSWebsocketDotNet.Types.OutputState.Started : OBSWebsocketDotNet.Types.OutputState.Stopped);
                 }
-          
                 
                 if( vcamstatus != null && vcamstatus.IsActive )
                 {
@@ -182,8 +228,12 @@
                 {
                     this.OnObsVirtualCameraStopped(this, e);
                 }
-                
+
                 this.OnObsStudioModeStateChange(this, studioModeStatus);
+
+                this.OnObsSceneCollectionListChanged(this, e);
+
+                this.OnObsSceneCollectionChanged(this, e);
             });
 
         }
@@ -195,6 +245,9 @@
             this.VirtualCameraStarted -= this.OnObsVirtualCameraStarted;
             this.VirtualCameraStopped -= this.OnObsVirtualCameraStopped;
             this.StudioModeSwitched -= this.OnObsStudioModeStateChange;
+
+            this.SceneCollectionListChanged -= this.OnObsSceneCollectionListChanged;
+            this.SceneCollectionChanged -= this.OnObsSceneCollectionChanged;
 
             //Note, invoking 
             this.EvtAppDisconnected?.Invoke(sender, e);
