c68f730d763d18ffc7822c67bd60c1be20cd593e
Andrei Laperie <andrei.laperie@loupedeck.com>
2022-11-04 17:01:26 +0200

    added start and stop methods

--- GenStreamPlugin/src/GenStreamPlugin/Proxy/GenStreamProxy.Recording.cs
+++ GenStreamPlugin/src/GenStreamPlugin/Proxy/GenStreamProxy.Recording.cs
@@ -13,9 +13,20 @@
         public event EventHandler<EventArgs> AppEvtRecordingOn;
         public event EventHandler<EventArgs> AppEvtRecordingOff;
 
-        //FIXME: Provide customized images for starting/started... -- For that, create special event handler on Action side. 
+        public class IntParamArgs : EventArgs
+        {
+            public IntParamArgs(Int32 v) => this.State = v;
+
+            public Int32 State { get; set; }
+        }
+
+        public event EventHandler<IntParamArgs> AppEvtRecordingStateChange;
+
+        // FIXME: Provide customized images for starting/started... -- For that, create special event handler on Action side.
         private void OnObsRecordingStateChange(OBSWebsocket sender, OBSWebsocketDotNet.Types.OutputState newState)
         {
+            this.Trace($"OBS Recording state change, new state is {newState}");
+            
             if ((newState == OBSWebsocketDotNet.Types.OutputState.Started) || (newState == OBSWebsocketDotNet.Types.OutputState.Starting))
             {
                 this.AppEvtRecordingOn?.Invoke(this, new EventArgs());
@@ -24,12 +35,36 @@
             {
                 this.AppEvtRecordingOff?.Invoke(this, new EventArgs());
             }
+
+            this.AppEvtRecordingStateChange?.Invoke(this, new IntParamArgs((Int32)newState));
         }
         public void AppToggleRecording()
         {
             if (this.IsAppConnected)
             {
+                this.Trace("Toggle recording");
+
                 Helpers.TryExecuteSafe(() => this.ToggleRecording());
+            }
+        }
+
+        public void AppStartRecording()
+        {
+            if (this.IsAppConnected)
+            {
+                this.Trace("Start recording");
+
+                Helpers.TryExecuteSafe(() => this.StartRecording());
+            }
+        }
+
+        public void AppStopRecording()
+        {
+            if (this.IsAppConnected)
+            {
+                this.Trace("Stop recording");
+
+                Helpers.TryExecuteSafe(() => this.StopRecording());
             }
         }
 
