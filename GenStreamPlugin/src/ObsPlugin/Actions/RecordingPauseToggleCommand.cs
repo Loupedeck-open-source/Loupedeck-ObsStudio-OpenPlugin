50eb9c687639c62008f57cdbf8b8a761105169fd
Andrei Laperie <andrei.laperie@loupedeck.com>
2022-11-11 19:41:18 +0200

    added Recording Pause, Transition and Save Replay Buffer commands
    added new commands and respective proxy and image files

---
+++ GenStreamPlugin/src/ObsPlugin/Actions/RecordingPauseToggleCommand.cs
@@ -0,0 +1,84 @@
+﻿namespace Loupedeck.ObsPlugin.Actions
+{
+    using System;
+
+    public class RecordingPauseToggleCommand : GenericOnOffSwitch
+    {
+        private ObsAppProxy Proxy => (this.Plugin as ObsPlugin).Proxy;
+
+        public RecordingPauseToggleCommand()
+                : base("Recording Pause", "Pauses/Resumes recording", /*no group*/"",
+                new String[] {
+                    "Command unavailable",
+                    "Pause recording",
+                    "Resume recording"
+                },
+                new String[] {
+                  "Loupedeck.ObsPlugin.icons.SoftwareNotFound.png",
+                  "Loupedeck.ObsPlugin.icons.STREAM_RecordPause.png",
+                  "Loupedeck.ObsPlugin.icons.STREAM_RecordResume.png"
+                })
+        {
+        }
+
+        protected void OnAppRecordingStarted(Object sender, EventArgs e)
+        {
+            this.SetCurrentState(0); 
+            this.IsEnabled = true;
+        }
+
+        protected void OnAppRecordingStopped(Object sender, EventArgs e)
+        {
+            this.IsEnabled = false;
+            this.SetCurrentState(0); 
+        }
+
+        protected void OnAppRecordingResumed(Object sender, EventArgs e)
+        {
+            //Note this can be called from OnLoad as well (OffEvent) in which case we check if we are InRecording
+            this.IsEnabled = this.Proxy.InRecording;
+            this.ActionImageChanged();
+        }
+
+        protected void OnAppRecordingPaused(Object sender, EventArgs e)
+        {
+            this.ActionImageChanged();
+        }
+
+        protected override void ConnectAppEvents(EventHandler<EventArgs> onEvent, EventHandler<EventArgs> offEvent)
+        {
+            this.Proxy.AppEvtRecordingResumed += onEvent;
+            this.Proxy.AppEvtRecordingPaused += offEvent;
+            this.Proxy.AppEvtRecordingOff += this.OnAppRecordingStopped;
+            this.Proxy.AppEvtRecordingOn += this.OnAppRecordingStarted;
+            this.Proxy.AppEvtRecordingPaused += this.OnAppRecordingPaused;
+            this.Proxy.AppEvtRecordingResumed += this.OnAppRecordingResumed;
+        }
+
+        protected override void DisconnectAppEvents(EventHandler<EventArgs> onEvent, EventHandler<EventArgs> offEvent)
+        {
+            this.Proxy.AppEvtRecordingResumed -= onEvent;
+            this.Proxy.AppEvtRecordingPaused -= offEvent;
+            this.Proxy.AppEvtRecordingOff -= this.OnAppRecordingStopped;
+            this.Proxy.AppEvtRecordingOn -= this.OnAppRecordingStarted;
+            this.Proxy.AppEvtRecordingPaused -= this.OnAppRecordingPaused;
+            this.Proxy.AppEvtRecordingResumed -= this.OnAppRecordingResumed;
+
+        }
+
+        protected override void RunCommand(String actionParameter)
+        {
+            if(this.TryGetCurrentStateIndex(out var currentState))
+            {
+                if(currentState == 0)
+                {
+                    this.Proxy.AppPauseRecording();
+                }
+                else
+                {
+                    this.Proxy.AppResumeRecording();
+                }
+            }
+        }
+    }
+}
---
+++ GenStreamPlugin/src/ObsPlugin/Actions/ReplayBufferSaveCommand.cs
@@ -0,0 +1,49 @@
+﻿namespace Loupedeck.ObsPlugin.Actions
+{
+    using System;
+
+    public class ReplayBufferSaveCommand: PluginDynamicCommand
+    {
+        private const String IMGAction = "Loupedeck.ObsPlugin.icons.STREAM_SaveReplayBuffer.png";
+
+        private ObsAppProxy Proxy => (this.Plugin as ObsPlugin).Proxy;
+
+         public ReplayBufferSaveCommand() : base(displayName: "Save Replay Buffer", description: "Flush and save the contents of the Replay Buffer to disk. ", groupName: null) { }
+
+        protected override Boolean OnLoad()
+        {
+            this.Proxy.EvtAppConnected += this.OnAppConnected;
+            this.Proxy.EvtAppDisconnected += this.OnAppDisconnected;
+
+            this.Proxy.AppEvtReplayBufferOff += this.OnAppReplayBufferOff;
+            this.Proxy.AppEvtReplayBufferOn += this.OnAppReplayBufferOn;
+            this.IsEnabled = false;
+
+            return true;
+        }
+
+        protected override Boolean OnUnload()
+        {
+            this.Proxy.EvtAppConnected -= this.OnAppConnected;
+            this.Proxy.EvtAppDisconnected -= this.OnAppDisconnected;
+            this.Proxy.AppEvtReplayBufferOff -= this.OnAppReplayBufferOff;
+            this.Proxy.AppEvtReplayBufferOn -= this.OnAppReplayBufferOn;
+
+            return true;
+        }
+
+        private void OnAppConnected(Object sender, EventArgs e) => this.OnAppReplayBufferOff(sender, e);
+        
+        private void OnAppReplayBufferOn(Object sender, EventArgs e) => this.IsEnabled = true;
+        
+        private void OnAppReplayBufferOff(Object sender, EventArgs e) => this.IsEnabled = false;
+
+        private void OnAppDisconnected(Object sender, EventArgs e) => this.OnAppReplayBufferOff(sender, e);
+
+        protected override BitmapImage GetCommandImage(String actionParameter, PluginImageSize imageSize) => EmbeddedResources.ReadImage(IMGAction);
+
+        protected override void RunCommand(String actionParameter) => this.Proxy.AppSaveReplayBuffer();
+
+
+    }
+}
