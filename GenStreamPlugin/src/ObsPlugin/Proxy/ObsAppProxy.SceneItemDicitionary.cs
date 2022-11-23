9309bd9110e81cf33d736e248342d54019d428eb
Andrei Laperie <andrei.laperie@loupedeck.com>
2022-11-23 23:25:22 +0200

    optimized addtodictionary

--- GenStreamPlugin/src/ObsPlugin/Proxy/ObsAppProxy.SceneItemDicitionary.cs
+++ GenStreamPlugin/src/ObsPlugin/Proxy/ObsAppProxy.SceneItemDicitionary.cs
@@ -2,8 +2,7 @@
 {
     using System;
     using System.Collections.Generic;
-
-    using OBSWebsocketDotNet;
+    using OBSWebsocketDotNet.Types;
 
     /// <summary>
     /// Proxy to OBS websocket server, for API reference see
@@ -11,40 +10,6 @@
     /// </summary>
     public partial class ObsAppProxy
     {
-        // NOTE: The searching ONLY in the current scene
-        private Boolean AddSceneItemToDictionary(String sceneName, String sourceName)
-        {
-            foreach (var item in this.CurrentScene.Items)
-            {
-                if (item.SourceName == sourceName)
-                {
-                    return this.AddSceneItemToDictionary(sceneName, item);
-                }
-            }
-
-            ObsStudioPlugin.Trace($"Cannot get sceneItem for item {sourceName} of scene {sceneName}");
-
-            return false;
-        }
-
-        private Boolean AddSceneItemToDictionary(String sceneName, OBSWebsocketDotNet.Types.SceneItem item)
-        {
-            var sourceDictItem = SceneItemDescriptor.CreateSourceDictItem(this.CurrentSceneCollection, sceneName, item, this);
-
-            if (sourceDictItem != null)
-            {
-                this.AllSceneItems.Add(
-                    SceneItemKey.Encode(this.CurrentSceneCollection, sceneName, item.SourceName),
-                    sourceDictItem);
-                return true;
-            }
-            else
-            {
-                ObsStudioPlugin.Trace($"Cannot get props for item {item.SourceName} of scene {sceneName}");
-                return false;
-            }
-        }
-
         // Retreives all scene items for all scenes in current collection
         private void OnObsSceneCollectionChange_FetchSceneItems()
         {
@@ -66,10 +31,11 @@
                     var sceneItem = scene.Items.Find(x => x.SourceName == item.SourceName);
                     if (sceneItem != null)
                     {
-                        var sourceDictItem = SceneItemDescriptor.CreateSourceDictItem(this.CurrentSceneCollection, scene.Name, sceneItem, this, null, item);
+                        var sourceDictItem = SceneItemDescriptor.CreateSourceDictItem(this.CurrentSceneCollection, scene.Name, sceneItem, this,item);
                         if (sourceDictItem != null)
                         {
-                            this.AllSceneItems.Add(SceneItemKey.Encode(this.CurrentSceneCollection, scene.Name, item.SourceName), sourceDictItem);
+                            var key = SceneItemKey.Encode(this.CurrentSceneCollection, scene.Name, item.SourceName); 
+                            this.AllSceneItems.Add(key, sourceDictItem);
                         }
                         else
                         {
@@ -100,16 +66,16 @@
 
             public String SceneName;
 
-            public String SceneNameProp => this._sceneItemProps.ItemName;
+            public String SceneItemName => this._sceneItemProps != null ? this._sceneItemProps.ItemName : "Invalid_Item_Name";
 
-            public String SourceName => this._sceneItemDetails.SourceName;
+            public String SourceName => this._sceneItemDetails != null ? this._sceneItemDetails.SourceName : "Invalid_Source_Name";
 
             public Boolean Visible { get => this._sceneItemProps.Visible; set => this._sceneItemProps.Visible = value; }
 
             // private readonly volumeinfo;
             // private readonly OBSWebsocketDotNet.Types.SceneItem _sceneItem;
-            private readonly OBSWebsocketDotNet.Types.SceneItemDetails _sceneItemDetails;
-            private readonly OBSWebsocketDotNet.Types.SceneItemProperties _sceneItemProps;
+            private readonly SceneItemDetails _sceneItemDetails;
+            private readonly SceneItemProperties _sceneItemProps;
 
             /// <summary>
             /// Creates a single Source Dictionary item, optionally feching SceneItemProperties  and SceneItemDetails
@@ -121,28 +87,30 @@
             /// <param name="in_props">properties</param>
             /// <param name="in_details">details</param>
             /// <returns></returns>
-            public static SceneItemDescriptor CreateSourceDictItem(String in_collection, String in_sceneName, OBSWebsocketDotNet.Types.SceneItem in_sceneItem, OBSWebsocketDotNet.OBSWebsocket obs,
-                                    OBSWebsocketDotNet.Types.SceneItemProperties in_props = null, OBSWebsocketDotNet.Types.SceneItemDetails in_details = null)
+            public static SceneItemDescriptor CreateSourceDictItem(String in_collection, String in_sceneName, SceneItem in_sceneItem, OBSWebsocketDotNet.OBSWebsocket obs, SceneItemDetails in_details = null)
             {
                 try
                 {
-                    var props = in_props ?? obs.GetSceneItemProperties(in_sceneItem.SourceName, in_sceneName);
+                    var props = obs.GetSceneItemProperties(in_sceneItem.SourceName, in_sceneName);
+                    
                     var details = in_details;
 
-                    if (in_details == null)
+                    if (details == null)
                     {
-                        var list = obs.GetSceneItemList(in_sceneName);
-                        foreach (var detail in list)
+                        //Getting information about all scene' sources from OBS and selecting ours
+                        var sceneItemList = obs.GetSceneItemList(in_sceneName);
+                        
+                        foreach (var sceneItem in sceneItemList)
                         {
-                            if (detail.SourceName == in_sceneItem.SourceName)
+                            if (sceneItem.SourceName == in_sceneItem.SourceName)
                             {
-                                in_details = detail;
+                                details = sceneItem;
                                 break;
                             }
                         }
                     }
 
-                    if (in_details == null)
+                    if (details == null)
                     {
                         throw new Exception("Cannot find details for source");
                     }
@@ -153,25 +121,18 @@
                 }
                 catch (Exception ex)
                 {
-                    ObsStudioPlugin.Trace($"Warning: Exception {ex.Message} in creating source item for item {in_sceneItem.SourceName} of scene {in_sceneName}  ");
+                    ObsStudioPlugin.Trace($"Warning: Exception: {ex.Message} in creating source item for item '{in_sceneItem.SourceName}' of scene '{in_sceneName}'");
                 }
 
                 return null;
             }
 
-            protected SceneItemDescriptor(String coll, String scene, OBSWebsocketDotNet.Types.SceneItem item, OBSWebsocketDotNet.Types.SceneItemDetails details, OBSWebsocketDotNet.Types.SceneItemProperties props)
+            protected SceneItemDescriptor(String coll, String scene, SceneItem item, SceneItemDetails details, SceneItemProperties props)
             {
                 this.CollectionName = coll;
-
-                // this._sceneItem = item;
                 this.SceneName = scene;
                 this._sceneItemDetails = details;
                 this._sceneItemProps = props;
-
-                if (scene != this.SceneNameProp)
-                {
-                    ObsStudioPlugin.Trace($"SourceDictItem ctor: Scene name {scene} Scene Name in details: {this.SceneNameProp}");
-                }
             }
         }
     }
