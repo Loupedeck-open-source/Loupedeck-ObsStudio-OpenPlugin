ff715d1ef8a1887db17e12713ebc7a0e168d377c
Andrei Laperie <andrei.laperie@loupedeck.com>
2022-11-24 11:04:16 +0200

    All support classes have a new home

---
+++ GenStreamPlugin/src/ObsPlugin/Support/AudioSourceDescriptor.cs
@@ -0,0 +1,32 @@
+﻿namespace Loupedeck.ObsStudioPlugin
+{
+    using System;
+    using OBSWebsocketDotNet;
+
+    public class AudioSourceDescriptor
+    {
+        public Boolean SpecialSource;
+        public Boolean Muted;
+        public Single Volume;
+
+        public AudioSourceDescriptor(String name, OBSWebsocket that, Boolean isSpecSource = false)
+        {
+            this.Muted = false;
+            this.Volume = 0;
+            this.SpecialSource = isSpecSource;
+
+            try
+            {
+                var v = that.GetVolume(name);
+                this.Muted = v.Muted;
+                this.Volume = v.Volume;
+            }
+            catch (Exception ex)
+            {
+                ObsStudioPlugin.Trace($"Exception {ex.Message} getting volume information for source {name}");
+            }
+        }
+    }
+
+
+}
---
+++ GenStreamPlugin/src/ObsPlugin/Support/EventArgs.cs
@@ -0,0 +1,47 @@
+﻿namespace Loupedeck.ObsStudioPlugin
+{
+    using System;
+
+    public class MuteEventArgs : EventArgs
+    {
+        public String SourceName;
+        public Boolean isMuted;
+        public MuteEventArgs(String name, Boolean muted) 
+        { 
+            this.SourceName = name; 
+            this.isMuted = muted; 
+        }
+    }
+
+    public class VolumeEventArgs : EventArgs
+    {
+        public String SourceName;
+        public Single Volume;
+        public Single VolumeDb;
+        public VolumeEventArgs(String name, Single vol, Single vol_db = 0)
+        {
+            this.SourceName = name;
+            this.Volume = vol;
+            this.VolumeDb = vol_db;
+        }
+    }
+
+    //Commonly used old-new arg class for 'onchange' events 
+    public class OldNewStringChangeEventArgs : EventArgs
+    {
+        public String Old;
+        public String New;
+        public OldNewStringChangeEventArgs(String old, String _new) 
+        { 
+            this.Old = old; 
+            this.New = _new; 
+        }
+    }
+
+    public class SourceNameEventArgs: EventArgs
+    {
+        public String SourceName;
+        public SourceNameEventArgs(String name) { this.SourceName = name; }
+
+    }
+}
