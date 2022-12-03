935ef4bf29dc87797b72610de1cb8fa70f5758fa
Andrei Laperie <andrei.laperie@loupedeck.com>
2022-12-03 12:36:37 +0200

    Screenshot command added

---
+++ ObsStudioPlugin/src/ObsPlugin/Actions/ScreenshotCommand.cs
@@ -0,0 +1,37 @@
+﻿namespace Loupedeck.ObsStudioPlugin.Actions
+{
+    using System;
+
+    internal class ScreenshotCommand : PluginDynamicCommand
+    {
+        private const String IMGAction = "Workspaces.png";
+
+        public ScreenshotCommand()
+            : base(displayName: "Screenshot",
+                   description: "Take a screenshot of currently active source with default parameters",
+                   groupName: "") => this.Name = "Screenshot";
+
+        protected override Boolean OnLoad()
+        {
+            ObsStudioPlugin.Proxy.AppConnected += this.OnAppConnected;
+            ObsStudioPlugin.Proxy.AppDisconnected += this.OnAppDisconnected;
+            this.IsEnabled = false;
+            return true;
+        }
+
+        protected override Boolean OnUnload()
+        {
+            ObsStudioPlugin.Proxy.AppConnected -= this.OnAppConnected;
+            ObsStudioPlugin.Proxy.AppDisconnected -= this.OnAppDisconnected;
+            return true;
+        }
+
+        private void OnAppConnected(Object sender, EventArgs e) => this.IsEnabled = true;
+
+        private void OnAppDisconnected(Object sender, EventArgs e) => this.IsEnabled = false;
+
+        protected override BitmapImage GetCommandImage(String actionParameter, PluginImageSize imageSize) => (this.Plugin as ObsStudioPlugin).GetPluginCommandImage(imageSize, IMGAction);
+
+        protected override void RunCommand(String actionParameter) => ObsStudioPlugin.Proxy.AppTakeScreenshot();
+    }
+}
---
+++ ObsStudioPlugin/src/ObsPlugin/Proxy/ObsAppProxy.Misc.cs
@@ -0,0 +1,43 @@
+﻿namespace Loupedeck.ObsStudioPlugin
+{
+    using System;
+    using System.Collections.Generic;
+
+    using OBSWebsocketDotNet;
+
+    /// <summary>
+    /// Proxy to OBS websocket server, for API reference see
+    /// https://github.com/obsproject/obs-websocket/blob/4.x-compat/docs/generated/protocol.md
+    /// </summary>
+    internal partial class ObsAppProxy
+    {
+        //TODO: This kind of stuff needs to be in plugin settings
+        static internal String DefaultScreenshotPath = "";
+
+        public void AppTakeScreenshot()
+        {
+            var currentScene = this.CurrentScene?.Name;
+
+            if (this.IsAppConnected && !String.IsNullOrEmpty(currentScene))
+            {
+                if (ObsAppProxy.DefaultScreenshotPath == "")
+                {
+                    ObsAppProxy.DefaultScreenshotPath = Environment.ExpandEnvironmentVariables("%USERPROFILE%\\Videos\\");
+                }
+
+                try
+                {
+                    //Generate unique filename 
+                    var filename = ObsAppProxy.DefaultScreenshotPath + "Screenshot-" + DateTime.Now.ToString("yyyyMMddHHmmssfff") + ".png";
+                    var resp = this.TakeSourceScreenshot((String)currentScene, null, filename, -1, -1);
+                    this.Plugin.Log.Info($"Screenshot taken and saved to {resp.ImageFile}");
+                }
+                catch (Exception ex)
+                {
+                    this.Plugin.Log.Error($"Exception {ex.Message} taking screenshot");
+                }
+            }
+        }
+
+    }
+}
