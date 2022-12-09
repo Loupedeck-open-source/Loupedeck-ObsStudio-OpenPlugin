5529ca4cda0b687d9a2fd5c5f0a8415cc054ca43
Andrei Laperie <andrei.laperie@loupedeck.com>
2022-12-09 10:17:44 +0200

    Added initial version of Universal switch

--- ObsStudioPlugin/src/ObsPlugin/Actions/UniversalStateSwitch.cs
+++ ObsStudioPlugin/src/ObsPlugin/Actions/UniversalStateSwitch.cs
@@ -2,22 +2,40 @@
 {
     using System;
 
-    internal class UniversalStateSwitch : ActionEditor
+    internal class UniversalStateSwitch : ActionEditorCommand
     {
+        //NOTE IT MUST START WITH LOWERCASE
+        private const String ActionNameControlName = "actionControlName";
+        private const String ActionActsControlName = "actionActsControlName";
 
         public UniversalStateSwitch()
-            : base(displayName: "Studio Mode Transition",
-                   description: "Changes your preview in Studio Mode to the active program scene",
-                   groupName: "") => this.Name = "Transition";
+        {
+
+            this.DisplayName = "Universal switch";
+            this.Description = "Sets whatever to whatever state you want";
+            this.GroupName = "";
+
+
+            this.ActionEditor.AddControl(
+                new ActionEditorListbox(name: ActionNameControlName, labelText: "What to switch:"));
+            
+            this.ActionEditor.AddControl(
+                new ActionEditorListbox(name: ActionActsControlName, labelText: "Select to what state to switch:"));
+/*
+            this.ActionEditor.AddControl(
+                new ActionEditorCheckbox(name: ActionActsControlName, labelText: "Select to what state to switch:")
+                    .SetDefaultValue(true)
+                );
+*/
+            this.ActionEditor.ListboxItemsRequested += this.OnActionEditorListboxItemsRequested;
+            this.ActionEditor.ControlValueChanged += this.OnActionEditorControlValueChanged;
+        }
 
         protected override Boolean OnLoad()
         {
             ObsStudioPlugin.Proxy.AppConnected += this.OnAppConnected;
             ObsStudioPlugin.Proxy.AppDisconnected += this.OnAppDisconnected;
 
-            ObsStudioPlugin.Proxy.AppEvtStudioModeOn += this.OnAppStudioModeOn;
-            ObsStudioPlugin.Proxy.AppEvtStudioModeOff += this.OnAppStudioModeOff;
-            this.IsEnabled = false;
             return true;
         }
 
@@ -25,21 +43,116 @@
         {
             ObsStudioPlugin.Proxy.AppConnected -= this.OnAppConnected;
             ObsStudioPlugin.Proxy.AppDisconnected -= this.OnAppDisconnected;
-            ObsStudioPlugin.Proxy.AppEvtStudioModeOn -= this.OnAppStudioModeOn;
-            ObsStudioPlugin.Proxy.AppEvtStudioModeOff -= this.OnAppStudioModeOff;
+
             return true;
         }
 
-        private void OnAppConnected(Object sender, EventArgs e) => this.OnAppStudioModeOff(sender, e);
+        protected override String GetCommandDisplayName(ActionEditorActionParameters actionParameters)
+        {
+            if( !actionParameters.TryGetString(ActionNameControlName, out var p1val))
+            {
+                p1val = "Unknown";
+            }
 
-        private void OnAppStudioModeOn(Object sender, EventArgs e) => this.IsEnabled = true;
+            if (!actionParameters.TryGetString(ActionActsControlName, out var p2val))
+            {
+                p2val = "Unknown";
+            }
 
-        private void OnAppStudioModeOff(Object sender, EventArgs e) => this.IsEnabled = false;
+            return "Ctrl -" + p1val + "  " + p2val;
 
-        private void OnAppDisconnected(Object sender, EventArgs e) => this.OnAppStudioModeOff(sender, e);
 
-        protected override BitmapImage GetCommandImage(String actionParameter, PluginImageSize imageSize) => (this.Plugin as ObsStudioPlugin).GetPluginCommandImage(imageSize, IMGAction);
+        }
 
-        protected override void RunCommand(String actionParameter) => ObsStudioPlugin.Proxy.AppRunTransition();
+
+        private void OnActionEditorControlValueChanged(Object sender, ActionEditorControlValueChangedEventArgs e)
+        {
+            
+            if (e.ControlName.EqualsNoCase(ActionNameControlName))
+            {
+                this.ActionEditor.ListboxItemsChanged(ActionActsControlName);
+            }
+
+
+            //e.ActionEditorState.SetDisplayName("something")
+
+            /*if (e.ControlName.EqualsNoCase(ActionActsControlName))
+            {
+                var listbox2ControlValue = e.ActionEditorState.GetControlValue(ActionActsControlName);
+   
+            }
+            */
+
+        }
+
+        class ToggleActionDescription
+        {
+            public String ToggleName;
+            public String ToggleDescription;
+            public String OnCommand;
+            public String OffCommand;
+            public ToggleActionDescription(String name, String description, String onCmd, String offCmd)
+            {
+                this.ToggleName = name;
+                this.ToggleDescription = description;
+                this.OnCommand = onCmd;
+                this.OffCommand = offCmd;
+            }
+            //TODO: Add run command
+        };
+
+        private static readonly ToggleActionDescription[] allActions =
+                new ToggleActionDescription[] {
+                        new ToggleActionDescription( "a1", "b1", "c1", "d1" ),
+                        new ToggleActionDescription( "a2", "b2", "c2", "d2" ),
+                        new ToggleActionDescription( "a3", "b3", "c3", "d3" ),
+                        new ToggleActionDescription( "a4", "b4", "c4", "d4" ) };
+
+        private void OnActionEditorListboxItemsRequested(Object sender, ActionEditorListboxItemsRequestedEventArgs e)
+        {
+            if (e.ControlName.EqualsNoCase(ActionNameControlName))
+            {
+                foreach (var v in allActions)
+                {
+                    e.AddItem(v.ToggleName, v.ToggleName, v.ToggleDescription);
+                }
+            }
+            else if (e.ControlName.EqualsNoCase(ActionActsControlName))
+            {
+                // We get the control value from the 1st list box and generate the list accordingly
+                //FIXME REPLACE ALLACTIONS WITH THE PROPER CODE ON THE PROXY LEVEL, with events and on-and off- commands
+                var x = e.ActionEditorState.GetControlValue(ActionNameControlName);
+                foreach (var v in allActions)
+                {
+                    if (v.ToggleName == x)
+                    {
+                        e.AddItem(v.OffCommand, v.OffCommand, "Turns it off");
+                        e.AddItem(v.OnCommand, v.OnCommand, "Turns it on");
+
+                    }
+                }
+
+            }
+
+
+        }
+
+
+
+        private void OnAppConnected(Object sender, EventArgs e)
+        {
+        }
+        private void OnAppDisconnected(Object sender, EventArgs e)
+        {
+        }
+
+        //protected override BitmapImage GetCommandImage(String actionParameter, PluginImageSize imageSize) => (this.Plugin as ObsStudioPlugin).GetPluginCommandImage(imageSize, IMGAction);
+
+        protected override Boolean RunCommand(ActionEditorActionParameters actionParameters)
+        {
+            this.Plugin.Log.Info($"Run command for {actionParameters}");
+            return false;
+        }
+
     }
 }
