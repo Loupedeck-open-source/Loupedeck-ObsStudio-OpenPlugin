f1ad777043bf2e4bfec3f180ee462c1064be5af2
Andrei Laperie <andrei.laperie@loupedeck.com>
2022-12-15 14:57:30 +0200

    updated default fields for DLL creation

--- ObsStudioPlugin/src/ObsPlugin/Properties/AssemblyInfo.cs
+++ ObsStudioPlugin/src/ObsPlugin/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("OBS Studio plugin for Loupedeck")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("OBS Studio Plugin for Loupedeck")]
+[assembly: AssemblyDescription("Plugin allowing to control OBS Studio application with Loupedeck software")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("LoupeDeck Ltd.")]
-[assembly: AssemblyProduct("Loupedeck")]
-[assembly: AssemblyCopyright("Copyright")]
+[assembly: AssemblyProduct("ObsPlugin")]
+[assembly: AssemblyCopyright("2022 LoupeeDeck Ltd")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,4 +32,4 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("5.4.0.20000")]
