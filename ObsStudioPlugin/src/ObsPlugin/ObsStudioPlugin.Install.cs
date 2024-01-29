d7c260d65955b532366176f6b17d89a69a2e210d
Andrei Laperie <andrei.laperie@gmail.com>
2024-01-29 23:01:53 +0200

    trying to fix ini on install

--- ObsStudioPlugin/src/ObsPlugin/ObsStudioPlugin.Install.cs
+++ ObsStudioPlugin/src/ObsPlugin/ObsStudioPlugin.Install.cs
@@ -16,7 +16,24 @@
                 // Note, if this is not possible it is something really not good with LD installation
                 return false;
             }
-#warning "Here we detect if app is installed and modify ini file if needed"
+
+            this.Log.Info($"Install: OBS Installed: {this.ClientApplication.GetApplicationStatus() == ClientApplicationStatus.Installed}. Ini file exists/good:{this._iniFile.iniFileExists}/{this._iniFile.iniFileGood}");
+
+            if (this.ClientApplication.GetApplicationStatus() == ClientApplicationStatus.Installed
+                        && this._iniFile.iniFileExists
+                        && !this._iniFile.iniFileGood
+                        )
+            {
+                this.Log.Info("Install: OBS Installed but INI file is bad, fixing");
+
+                //Attempting to fix ini file if it is not good. Can only be done when app is not running (for Portable app we need to know its location first
+                this._iniFile.FixIniFile();
+            }
+
+            this.Update_PluginStatus();
+
+
+
             return true;
         }
 
