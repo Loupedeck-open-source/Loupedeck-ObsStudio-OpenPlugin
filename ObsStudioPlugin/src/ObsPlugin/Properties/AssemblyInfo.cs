1368fc97ba4d4b5d54cb1ceb819c8305f3a2b685
Andrei Laperie <andrei.laperie@gmail.com>
2024-05-19 21:09:26 +0300

    Added main plugin dll in front so that all assembly information is correct. Increased version and fixed assembly info

--- ObsStudioPlugin/src/ObsPlugin/Properties/AssemblyInfo.cs
+++ ObsStudioPlugin/src/ObsPlugin/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("LoupeDeck Ltd.")]
 [assembly: AssemblyProduct("ObsPlugin")]
-[assembly: AssemblyCopyright("2022 LoupeeDeck Ltd")]
+[assembly: AssemblyCopyright("2024 LoupeeDeck Ltd")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,4 +32,4 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("5.4.0.20000")]
+[assembly: AssemblyVersion("5.10.4")]
